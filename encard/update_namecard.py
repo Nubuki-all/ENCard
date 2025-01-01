1781fdbb1acafc8d8c1f3b34d1247b896bc93036
Pah. <121189493+Nubuki-all@users.noreply.github.com>
2025-01-02 00:45:32 +0100

    Update update_namecard.py

--- encard/update_namecard.py
+++ encard/update_namecard.py
@@ -1,41 +1,39 @@
-import ambr
+import aiohttp 
 import asyncio
 import json
 import os
 
-async def update():
-    async with ambr.AmbrAPI(cache_ttl=30) as client:
-        char = await client.fetch_characters()
-        nc = await client.fetch_namecards()
-    
-    char_list = {}
-    dir = os.path.dirname(__file__)
-    file = f"{dir}/src/assets/data/namecard.json"
-    for x in char:
-        char_list.update({x.id: x.name}) if not x.id in char_list else None
-    
-    out = {}
-    for id in list(char_list.keys()):
-        for x in nc:
-            if ((str(id)).startswith("10000005-") or (str(id)).startswith("10000007-")):
-                if x.id == 210001:
-                    pre_image, _e = os.path.splitext(x.icon)
-                    image = pre_image.replace("UI_NameCardIcon", "UI_NameCardPic")
-                    image += "_P.png"
-                    out.update({id: {"id": x.id, "icon": x.icon, "image": image}})
-                    break
-            elif x.type == 'bond' and x.name.startswith(char_list.get(id)):
-                pre_image, _e = os.path.splitext(x.icon)
-                image = pre_image.replace("UI_NameCardIcon", "UI_NameCardPic")
-                image += "_P.png"
-                out.update({id: {"id": x.id, "icon": x.icon, "image": image}})
-                break
-    for x in nc:
-        if x.id == 210192:
-            out.update({10000005: {"id": x.id, "icon": x.icon, "image": x.icon}})
-            out.update({10000007: {"id": x.id, "icon": x.icon, "image": x.icon}})
-    
-    with open(file, "w")as out_file:
-        json.dump(out, out_file, indent=4)
+async def fetch_data(url): 
+    async with aiohttp.ClientSession() as session: 
+        async with session.get(url) as response: 
+            if response.status == 200: 
+                data = await response.json() 
+                return data 
+            else: 
+                print(f"Error: {response.status}") 
+                return None
 
-    print("Successfully updated namecards.")
+dir = os.path.dirname(__file__)
+file = f"{dir}/src/assets/data/namecard.json"
+ 
+link_image = "https://gi.yatta.moe/assets/UI/namecard/{image}.png" 
+link_icon = "https://gi.yatta.moe/assets/UI/namecard/{icon}.png" 
+default_icon = "https://gi.yatta.moe/assets/UI/namecard/UI_NameCardIcon_0.png" 
+default_image = "https://gi.yatta.moe/assets/UI/namecard/UI_NameCardPic_0_P.png" 
+ 
+json_new = {} 
+ 
+async def main(): 
+    charter_list = await fetch_data("https://gi.yatta.moe/api/v2/en/avatar?vh=5303") 
+    for key in charter_list["data"]["items"]: 
+        data_charter = await fetch_data(f"https://gi.yatta.moe/api/v2/en/avatar/{key}?vh=5303") 
+        if data_charter["data"]["other"] is None: 
+            json_new[key] = {"id": 210001, "icon": default_icon, "image": default_image}        
+            continue 
+        output_string = data_charter["data"]["other"]["nameCard"]["icon"].replace("Icon_", "Pic_") 
+        output_string += "_P" 
+        json_new[key] = {"id": data_charter["data"]["other"]["nameCard"]["id"], "icon": link_icon.format(icon = data_charter["data"]["other"]["nameCard"]["icon"]), "image": link_image.format(image = output_string)}        
+
+
+    with open(file, "w")as out_file:
+        json.dump(json_new, out_file, indent=4)
